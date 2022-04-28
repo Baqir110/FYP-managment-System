0c00ee9c930e0c869fed1d073be7d88b62b99ffe
Baqir110 <76534576+Baqir110@users.noreply.github.com>
2022-04-28 03:19:11 +0300

    Delete connection.php

--- connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* these variables will use for connection to mysql across all pages */
-$server = "localhost";
-$username = "root";
-$password = "";
-$dbname = "globalfyp";
-$con = new MySQLi($server,$username,$password,$dbname);
-?>
